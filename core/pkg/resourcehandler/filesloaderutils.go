292b175273540b707c2df6cebbf69c1508dec94d
Kayd-06 <kunaldadlani25@gmail.com>
2026-05-06 18:45:32 +0530

    fix(core): reword warning logs for unknown objects and include IDs
    Signed-off-by: Kayd-06 <kunaldadlani25@gmail.com>

--- core/pkg/resourcehandler/filesloaderutils.go
+++ core/pkg/resourcehandler/filesloaderutils.go
@@ -14,14 +14,14 @@ func addWorkloadsToResourcesMap(allResources map[string][]workloadinterface.IMet
 	for i := range workloads {
 		groupVersionResource, err := k8sinterface.GetGroupVersionResource(workloads[i].GetKind())
 		if err != nil {
-			logger.L().Warning("failed to get GroupVersionResource for workload", helpers.String("kind", workloads[i].GetKind()), helpers.Error(err))
+			logger.L().Warning("unsupported/unmapped object kind", helpers.String("kind", workloads[i].GetKind()), helpers.String("id", workloads[i].GetID()), helpers.Error(err))
 			continue
 		}
 
 		if k8sinterface.IsTypeWorkload(workloads[i].GetObject()) {
 			w := workloadinterface.NewWorkloadObj(workloads[i].GetObject())
 			if groupVersionResource.Group != w.GetGroup() || groupVersionResource.Version != w.GetVersion() {
-				logger.L().Warning("workload GroupVersion mismatch", helpers.String("kind", workloads[i].GetKind()), helpers.String("expectedGroup", groupVersionResource.Group), helpers.String("actualGroup", w.GetGroup()), helpers.String("expectedVersion", groupVersionResource.Version), helpers.String("actualVersion", w.GetVersion()))
+				logger.L().Warning("workload GroupVersion mismatch", helpers.String("id", workloads[i].GetID()), helpers.String("kind", workloads[i].GetKind()), helpers.String("expectedGroup", groupVersionResource.Group), helpers.String("actualGroup", w.GetGroup()), helpers.String("expectedVersion", groupVersionResource.Version), helpers.String("actualVersion", w.GetVersion()))
 				continue
 			}
 		}
